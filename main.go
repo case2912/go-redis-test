d98314fb30bc2e4a7e33b1c090b061891406f3c3
case2912 <kento.shiroyama@gmail.com>
2019-08-13 23:51:31 +0900

    [Modify] http GET request でstatus code 200を返すエンドポイントを作成

--- main.go
+++ main.go
@@ -1,7 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
 
 func main() {
 	fmt.Printf("hello golang!")
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		type Ping struct {
+			Status int
+			Rssult string
+		}
+		ping := Ping{http.StatusOK, "ok"}
+
+		res, err := json.Marshal(ping)
+
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		w.Header().Set("Content-Type", "application/json")
+		w.Write(res)
+	})
+	http.ListenAndServe(":8080", nil)
 }
